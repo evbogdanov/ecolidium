fc375e5eacb73870356843c0ba605b9c4c639363
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-04-19 14:52:51 +0300

    Better validation

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,5 @@
 class Service < ApplicationRecord
-  validates :slug, format: { with: /\A[a-zа-яё-]+\z/,
-                             message: 'only allows letters in lower case and hyphen' }
+  validates :slug, format: { with: /\A[a-zа-яё-]{3,100}\z/,
+                             message: 'only allows letters in lower case and hyphen (3-100 characters)' }
+  validates :title, length: { in: 3..100 }
 end
